7ae0819cc6936af4cc48d785614475190d2ba501
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2025-05-26 14:04:52 +0100

    Access Database in Sever Components with Async Function

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,15 @@
-export default function Contact() {
+export default async function Contact() {
   console.log("Hey is this in the server or client?");
-  return <div> Contact page </div>;
+
+  // Requesting for dummy data from json file
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+  console.log(posts);
+
+  return (
+    <>
+      <div> Contact page </div>
+      <button>Click Me</button>
+    </>
+  );
 }
